0fd1a9a2f486eaded96e6bea834ac1e6d3bbb97f
Sam Girshovich <sam.girshovich@gmail.com>
2021-02-06 16:40:00 -0500

    delete js grammer

--- static/js/delete.js
+++ static/js/delete.js
@@ -1,5 +1,5 @@
 function deletePost(url) {
-	if (confirm('Are you sure you want to delete blog post?')) {
+	if (confirm('Are you sure you want to delete this blog post?')) {
 		window.location.pathname = url
 	}
 }
